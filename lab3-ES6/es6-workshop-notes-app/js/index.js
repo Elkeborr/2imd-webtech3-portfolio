d08f329f441df4ce59d38833cd3cfbd533dc57cd
Elkeborr <elkeborreij@student.thomasmore.be>
2019-03-14 19:11:52 +0100

    Probeersel 1

--- lab3-ES6/es6-workshop-notes-app/js/index.js
+++ lab3-ES6/es6-workshop-notes-app/js/index.js
@@ -1,37 +1,51 @@
 class Note {
   constructor(title) {
+
     this.title = title;
+    this.element = this.createElement(title);
     // HINT🤩 this.element = this.createElement(title);
+   
   }
-  
   createElement(title){
+
     let newNote = document.createElement('div');
-    
     // HINT🤩 a.addEventListener('click', this.remove.bind(newNote));
-    
+    newNote.innerHTML = "<p>"+this.notesText+"</p>"+
+    "<a href='#' class='card-remove'>"+"Remove"+"</a>";
+    newNote.classList.add("card");
+
+    a.addEventListener('click', this.remove.bind(newNote));
     return newNote;
   }
-  
   add(){
     // HINT🤩
     // this function should append the note to the screen somehow
+    document.querySelector('notes').appendChild(this.newNote);
   }
   
   saveToStorage(){
     // HINT🤩
     // localStorage only supports strings, not arrays
     // if you want to store arrays, look at JSON.parse and JSON.stringify
+
+ 
+const note = JSON.stringify(newNote);
+localStorage.setItem(note);
+
+
   }
   
   remove(){
     // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
     // in this function, 'this' will refer to the current note element
+
   } 
 }
 
 class App {
   constructor() {
     console.log("👊🏼 The Constructor!");
+
   
     // HINT🤩
     // clicking the button should work
@@ -45,6 +59,7 @@ class App {
     // HINT🤩
     // load all notes from storage here and add them to the screen
     // something like note.add() in a loop would be nice
+
   }
    
   createNote(e){
@@ -54,6 +69,9 @@ class App {
     // note.add();
     // note.saveToStorage();
     // this.reset();
+
+   note.add();
+   note.saveToStorage();
   }
   
   reset(){
